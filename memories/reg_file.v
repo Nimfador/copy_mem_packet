7cc16e2440c19098c0ccaa3f7ff2146db489914f
sqrlfirst <44490789+sqrlfirst@users.noreply.github.com>
2019-04-07 05:14:45 +0700

    mem_add
    add two parametrizable memories

---
+++ memories/reg_file.v
@@ -0,0 +1,27 @@
+module reg_file
+    #(
+        parameter               pBITS = 8,
+                                pWIDHT = 2
+    )
+    (
+        input wire              iclk,
+        input wire              iwr_en,
+        input wire [pWIDHT-1:0] iw_addr,
+        input wire [pWIDHT-1:0] ir_addr,
+        input wire [pBITS-1:0]  iw_data,
+        output wire [pBITS-1:0] or_data
+    );
+
+    // signal declaration
+    reg [pBITS-1:0] rarray_reg [2**pWIDHT-1:0];
+
+    // body
+    always @(posedge iclk ) begin
+        if (iwr_en) begin
+            rarray_reg[iw_addr] <= iw_data;    
+        end
+    end
+
+    assign or_data = rarray_reg[ir_addr];
+
+endmodule
