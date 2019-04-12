fd379661bb39c88a4dbbdcf4b268aff964a3bfd0
sqrlfirst <44490789+sqrlfirst@users.noreply.github.com>
2019-04-12 11:00:01 +0700

    Update copy_packet_to_mem_tb.v
    begin writing tb

--- memories/copy_packet_to_mem_tb.v
+++ memories/copy_packet_to_mem_tb.v
@@ -2,10 +2,10 @@ module copy_packet_to_mem_tb;
 
     reg                     iclk      ;         
     reg                     i_rst     ;
-    reg                     idv       ;
-    reg                     irx_d     ;         // 7:0
-    reg                     irx_er    ;
-    reg                     iFSM_state;
+    wire                     wdv       ;
+    wire [7:0]             wrx_d     ;         // 7:0
+    wire                    wrx_er    ;
+    wire [7:0]               wFSM_state;
     reg                     ir_addr   ;
     reg                     oempty    ;
     reg                     ofull     ;
@@ -20,12 +20,12 @@ module copy_packet_to_mem_tb;
         .pMEM_WIDTH             ()
     ) DUt
     (
-        .iclk                   (),
+        .iclk                   (iclk),
         .i_rst                  (),
-        .idv                    (),
-        .irx_d                  (),
-        .irx_er                 (),
-        .iFSM_state             (),
+        .idv                    (wdv),
+        .irx_d                  (wrx_d),
+        .irx_er                 (wrx_er),
+        .iFSM_state             (wFSM_state),
         .ir_addr                (),
         .oempty                 (),
         .ofull                  (),
@@ -33,6 +33,12 @@ module copy_packet_to_mem_tb;
         .olen_pac               ()
    );
 
-
+    module Ethernet_frame_analyzer_tb_w_io(
+        .iclk                   (iclk), 
+        .o_fsm_state            (wFSM_state),
+        .o_rx_data              (wrx_d),
+        .o_rx_er                (wrx_er),
+        .o_rx_dv                (wdv)
+    );
 
 endmodule
