cc4e18e81b35c86b593f0490d618d0e486246753
ravil99 <48554221+ravil99@users.noreply.github.com>
2019-04-24 19:22:17 +0700

    Version after fixing with DIma 24.04.19

--- MAC/MAC_memory.v
+++ MAC/MAC_memory.v
@@ -5,19 +5,21 @@ module MAC_memory
                                 pDATA_WIDTH=8,
                                 pTIME = 9,         //Capacity for save time = 300 seconds 
                                 pMAX_PACKET_LENGHT = 1536,
-                                pONE_SECOND = 125000000
+                                pONE_SECOND = 32768
     )
     (
         input wire                          iclk,
+        input wire                          i_dv,
         input wire [pADRESS-1:0]            i_port_num,
         input wire [pDATA_WIDTH-1:0]        irx_d,
         input wire [2:0]                    iFSM_state,
         output reg [pADRESS-1:0]            o_port_num
     );
 
-    reg [pADRESS-1:0]                       r_port_num [$clog2(pSLOTS)-1:0] ='{default: 'b0};
-    reg [pTIME-1:0]                         r_time [$clog2(pSLOTS)-1:0] ='{default: 'd300};
-    reg [$clog2(pSLOTS)-1:0]                r_adress ='0; 
+    reg [pADRESS-1:0]                       r_port_num [pSLOTS-1:0] ='{default: 'b0};
+    reg [pTIME-1:0]                         r_time [pSLOTS-1:0] ='{default: 'd300};
+    reg [$clog2(pSLOTS)-1:0]                r_adress ='0;
+    reg [$clog2(pSLOTS)-1:0]                r_read_adress ='0; 
 
     reg [$clog2(pMAX_PACKET_LENGHT)-1:0]    r_counter_len ='0;
     reg [5:0]                               r_MAC_higher ='0; 
@@ -30,23 +32,25 @@ module MAC_memory
 
 
     always @(posedge iclk) begin
-    r_FBC<=r_FBC+1;
-    if (r_FBC==27'd125000000)
+    if (r_FBC==15'd32768) 
        r_FBC<=0;
-       r_d_counter<='0; 
+    r_FBC<=r_FBC+1;
     end 
 
 
     always @(posedge iclk) begin                        //Getting MAC adress
-        if ((iFSM_state!==3'b000)|(iFSM_state!==3'b001)|(iFSM_state!==3'b111)) 
+        if ((iFSM_state!==3'b000)&(iFSM_state!==3'b001)&(iFSM_state!==3'b111)) 
             r_counter_len<=r_counter_len+1;       
         if ((iFSM_state==3'b100)|(iFSM_state==3'b101)|(iFSM_state==3'b110)) begin 
         case (r_counter_len)
-        'd10: r_MAC_higher<=irx_d [5:0];    //write Adress
-        'd11: r_MAC_lower<=irx_d;
-        'd12: r_adress<={r_MAC_higher,r_MAC_lower};
-        'd13: r_write_en<=1'b1;
-        'd14: r_write_en<=1'b0;
+        'd11: r_MAC_higher<=irx_d [5:0];    //write Adress
+        'd12: r_MAC_lower<=irx_d;
+        'd13: r_adress<={r_MAC_higher,r_MAC_lower};
+        'd14: r_write_en<=1'b1;
+        'd15: begin
+                r_write_en<=1'b0;
+                r_read_adress<=r_adress;
+                end
         endcase
         end
         if (iFSM_state==3'b111) 
@@ -58,13 +62,16 @@ module MAC_memory
             r_port_num[r_adress] <= i_port_num;
             r_time[r_adress] <= 9'd300;
         end
-        if  (r_FBC > 27'd124983615) begin
+        else if  (r_FBC > 'd16383) begin
             r_d_counter<=r_d_counter+1;
             r_time[r_d_counter]<=r_time[r_d_counter]-1;
         end
+        o_port_num<=r_port_num[r_read_adress];
         end
 
     endmodule
 
-
+//Секудный таймер на PSP
+//Буфер на 2 такта на указатель записи и чтения
+//
     
