af6e39b2e9a9cddefc91dca392145199223f1a3b
sqrlfirst <44490789+sqrlfirst@users.noreply.github.com>
2019-04-26 10:55:33 +0700

    Mac_mem 1_var
    there is several function as :
    -Reading & writing value to mem
    -time decreasing

--- MAC_mem/mac_mem.sv
+++ MAC_mem/mac_mem.sv
@@ -1,19 +1,20 @@
 module mac_mem 
     #(
-        parameter   pTIME = 300
+        parameter   pTIME        = 300,
                     pCLK_PER_SEC = 125_000_000,
-                    pNUM_PORTS = 4
-                    pADDR_WIDTH = 14,
-                    pDATA_DEPTH = 2**pADDR_WIDTH,
+                    pNUM_PORTS   = 4,
+                    pADDR_WIDTH  = 14,
+                    pDATA_DEPTH  = 2**pADDR_WIDTH,
     )
     (
         input wire                              iclk,
         input wire [$clog2(pNUM_PORTS)-1:0]     ipnum,          
         input wire [pADDR_WIDTH-1:0]            isa,
         input wire [pADDR_WIDTH-1:0]            ida,
+        input wire                              iwr_en,
 
         output reg [$clog2(pNUM_PORTS)-1:0]     opnum,
-        output                                  oready,
+        output                                  oready
     );
 
     // register for inner sram module
@@ -25,7 +26,6 @@ module mac_mem
 
     // general counter for controling time
     reg [$clog2(pCLK_PER_SEC)-1:0]              rtimer = pCLK_PER_SEC;
-    reg [$clog2(pDATA_DEPTH*2)-1:0]             rtimer2;                       // Maybe not needed
 
     sram 
     #(
@@ -47,37 +47,29 @@ module mac_mem
     end
 
     always @(posedge iclk) begin
-        if (rtimer > pDATA_DEPTH*2) begin
-            
+        if (rtimer > pDATA_DEPTH*2) begin           // Write SA value to mem
+            if(iwr_en) begin                           
+                rwr_en <= 1;
+                raddr_wr <= i_addr_wr;
+                rdata_wr[$clog2(pNUM_PORTS)+$clog2(pTIME)-1:$clog2(pTIME)-2] <= {ipnum, pTIME};
+            end
+            raddr_rd <= i_addr_r;                   // read DA value from mem 
         end
-        else  begin
-            if(rtimer % 2) begi
+        else  begin                                 // decrement time 
+            if(rtimer % 2) begin                    // update time value
                 rwr_en <= 0;
                 raddr_rd <= rtimer/2;
                 raddr_wr <= rtimer/2;
-                rdata_wr <= {raddr_rd[$clog2(pNUM_PORTS)+$clog2(pTIME)-1:$clog2(pTIME)-2],
-                             raddr_rd[$clog2(pTIME)-1:0]-1};
+                if(raddr_rd[$clog2(pTIME)-1:0] != 0)
+                    rdata_wr <= {raddr_rd[$clog2(pNUM_PORTS)+$clog2(pTIME)-1:$clog2(pTIME)-2],
+                                 raddr_rd[$clog2(pTIME)-1:0]-1};
             end
             else begin
                 rwr_en <= 1;
             end
         end
     end
-
-
-
-
-
-    // Write SA value to mem
-
-    // read DA value from mem 
-
-    // update time value 
-
-    always@(posedge iclk) begin 
-
-    end
-
-
     // output value for port
+    assign o_data = rdata_rd[$clog2(pNUM_PORTS)+$clog2(pTIME)-1:$clog2(pTIME)-2];
+
 endmodule 
